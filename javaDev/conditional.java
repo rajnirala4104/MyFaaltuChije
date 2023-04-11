1ff8f5ff80e0dd0a5b1d363f031ee36896ccf8c3
Raj_Nirala <83572606+rajnirala4104@users.noreply.github.com>
2023-04-11 23:05:13 +0530

    my java file added

---
+++ javaDev/conditional.java
@@ -0,0 +1,16 @@
+import java.util.Scanner ;
+public class conditional{
+    public static void main(String args[]){
+        System.out.println("Enter your Age: ");
+        Scanner sc =  new Scanner(System.in);
+        int userAge = sc.nextInt();
+        if(userAge < 18){
+            System.out.println("Sorry you can't vote...");
+        }else if(userAge >= 80){
+            System.out.println("You're not in world bro");
+        }
+        else{
+            System.out.println("You can vote");
+        }
+    }
+}
