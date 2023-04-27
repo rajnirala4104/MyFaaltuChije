d989a2cef10104feb961847490798197948ded34
Raj_Nirala <83572606+rajnirala4104@users.noreply.github.com>
2023-04-27 16:41:45 +0530

    more animation added using js

--- task_2/js/index.js
+++ task_2/js/index.js
@@ -1,7 +1,25 @@
 //------popup functionality-----
 const popup = document.querySelector('.popup');
-const popupFunc = () => popup.classList.toggle('d-none')
+const popupFunc = () => {
+    if("d-none" in sideBar.classList){
+        popup.classList.toggle('d-none');
+    }
+    else{
+        sideBar.classList.toggle('d-none');
+        popup.classList.toggle('d-none')
+    }
+}
 
 const sideBar = document.querySelector('.sideBar')
-const sideBarFunc = ()=>sideBar.classList.toggle('d-none');
+const sideBarFunc = ()=>{
+    if("d-none" === sideBar.classList[1]){
+        sideBar.style.animation = "moveLeftToRight 2s"
+        sideBar.classList.toggle('d-none');
+    }else{
+        sideBar.style.animation = "moveRightToLeft 2s"
+        sideBar.classList.toggle('d-none');
+    }
+
+}
+
 
