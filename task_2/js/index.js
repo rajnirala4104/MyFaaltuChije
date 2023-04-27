541b993e6aecd1cc5dd2356ae19d44bd305289cc
Raj_Nirala <83572606+rajnirala4104@users.noreply.github.com>
2023-04-27 15:45:02 +0530

    Animation Added

--- task_2/js/index.js
+++ task_2/js/index.js
@@ -1,3 +1,7 @@
 //------popup functionality-----
 const popup = document.querySelector('.popup');
 const popupFunc = () => popup.classList.toggle('d-none')
+
+const sideBar = document.querySelector('.sideBar')
+const sideBarFunc = ()=>sideBar.classList.toggle('d-none');
+
