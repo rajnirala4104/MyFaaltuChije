753a0412d104877be12075c370f73a1684e703de
Raj_Nirala <83572606+rajnirala4104@users.noreply.github.com>
2023-05-17 00:42:10 +0530

    delete javaDev folder

--- javaDev/logicBuilding/prob1.py
+++
@@ -1,7 +0,0 @@
-
-#output should -> 7,1,6,2,5,3,4
-arr = [1,2,3,4,5,6,7]
-
-for i in arr:
-    theValue = arr[len(arr)-1]*arr[len(arr)-2]+arr[0]
-    print(theValue%arr[len(arr)-1])
--- javaDev/logicBuilding/romanToInt.java
+++
@@ -1,5 +0,0 @@
-class romanToInt{
-    public static void main(String[] args){
-        System.out.println("checking...");
-    }
-}
