c471c757a053968f5734db105a6032975b296d45
Raj_Nirala <83572606+rajnirala4104@users.noreply.github.com>
2023-11-29 20:47:44 +0530

    tried to do something but happend something else

--- learningTypeScript/litTasks/calculator.js
+++ learningTypeScript/litTasks/calculator.js
@@ -5,9 +5,36 @@ var showCommands = document.getElementById('showCommands');
 runBtn === null || runBtn === void 0 ? void 0 : runBtn.addEventListener('click', function () {
     var userCommandValue = userCommand.value;
     console.log(userCommandValue.split(" "));
-    var constantOprations = ["+", "-", "/", "*", "**"];
-    userCommandValue.split(" ").forEach((function (singleArraysData) {
-        console.log(constantOprations.includes(singleArraysData));
-        // console.log(Number(singleArraysData))
+    var constantsOpration = ["+", "-", "/", "*", "**"];
+    userCommandValue.split(" ").map((function (singleArraysData) {
+        console.log(singleArraysData);
     }));
 });
+while (true) {
+    var firstNumber = prompt("Enter the Frist number :");
+    var op = prompt("Enter the Oprations, e.g: +, -, *, /, etc ");
+    var secondNumber = prompt("Enter the second value: ");
+    var askRepeat = prompt("press C for continue and X for not");
+    if (askRepeat === "x" || askRepeat === "X" || firstNumber === "X" || firstNumber === "x" || secondNumber === "X" || secondNumber === "x" || op === "X" || op === "x")
+        break;
+    if (askRepeat === "c" || askRepeat === "C")
+        continue;
+    if (op === "+") {
+        alert(Number(firstNumber) + Number(secondNumber));
+    }
+    else if (op === "-") {
+        alert(Number(firstNumber) - Number(secondNumber));
+    }
+    else if (op === "*") {
+        alert(Number(firstNumber) * Number(secondNumber));
+    }
+    else if (op === "*") {
+        alert(Number(firstNumber) / Number(secondNumber));
+    }
+    else if (op === "**") {
+        alert(Math.pow(Number(firstNumber), Number(secondNumber)));
+    }
+    else {
+        alert("i think you've entered worng opration");
+    }
+}
--- learningTypeScript/litTasks/calculator.ts
+++ learningTypeScript/litTasks/calculator.ts
@@ -7,9 +7,39 @@ const showCommands = document.getElementById('showCommands')
 runBtn?.addEventListener('click', () => {
     const userCommandValue = (userCommand as HTMLInputElement).value
     console.log(userCommandValue.split(" "));
-    const constantOprations = ["+", "-", "/", "*", "**"];
-    userCommandValue.split(" ").forEach((singleArraysData => {
-        console.log(constantOprations.includes(singleArraysData))
-        // console.log(Number(singleArraysData))
+    const constantsOpration = ["+", "-", "/", "*", "**"];
+    userCommandValue.split(" ").map((singleArraysData => {
+        console.log(singleArraysData)
     }))
 })
+
+
+while (true) {
+    const firstNumber = prompt("Enter the Frist number :");
+    const op = prompt("Enter the Oprations, e.g: +, -, *, /, etc ")
+    const secondNumber = prompt("Enter the second value: ");
+
+    if (op === "+") {
+        alert(Number(firstNumber) + Number(secondNumber))
+    }
+    else if (op === "-") {
+        alert(Number(firstNumber) - Number(secondNumber))
+    }
+    else if (op === "*") {
+        alert(Number(firstNumber) * Number(secondNumber))
+    }
+    else if (op === "*") {
+        alert(Number(firstNumber) / Number(secondNumber))
+    }
+    else if (op === "**") {
+        alert(Number(firstNumber) ** Number(secondNumber))
+    } else {
+        alert("i think you've entered worng opration")
+    }
+
+    const askRepeat = prompt("press C for continue and X for not")
+    if (askRepeat === "x" || askRepeat === "X" || firstNumber === "X" || firstNumber === "x" || secondNumber === "X" || secondNumber === "x" || op === "X" || op === "x") break
+    if (askRepeat === "c" || askRepeat === "C") continue
+
+}
+
