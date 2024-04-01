60492235ce65a211af28a8ae17165a6fde335b82
Raj_Nirala <83572606+rajnirala4104@users.noreply.github.com>
2024-04-01 13:20:11 +0530

    learnt some concept of c++

---
+++ learn/C++/conditions.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+      // cout << "Enter you age: ";
+
+      // int age;
+      // cin >> age;
+
+      // if (age < 18)
+      // {
+      //       cout << "Oops!! sorry You can't drive the car " << endl;
+      // }
+      // else
+      // {
+      //       cout << "You can drive the car" << endl;
+      // }
+
+      // --------------- problem for practice -------
+      // which one is greates number
+
+      // int a, b, c;
+      // cin >> a >> b >> c;
+      // if (a < b)
+      // {
+      //       if (b < c)
+      //       {
+      //             cout << "C is the greates number you've entered " << c << endl;
+      //       }
+      //       else
+      //       {
+      //             cout << "B is the greates number you've entered " << b << endl;
+      //       }
+      // }
+      // else
+      // {
+      //       if (a < c)
+      //       {
+      //             cout << "C is the greates number you've entered " << c << endl;
+      //       }
+      //       else
+      //       {
+      //             cout << "A is the greates number you've entered " << a << endl;
+      //       }
+      // }
+
+      // ------ second problem ----------
+      // even and odd
+
+      cout << "Enter a number" << endl;
+
+      int num;
+      cin >> num;
+
+      if (num % 2 == 0)
+      {
+            cout << "You've entered EVEN" << endl;
+      }
+      else
+      {
+            cout << "You've entered ODD" << endl;
+      }
+      return 0;
+}
---
+++ learn/C++/loops.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+      // ----- print the table -----
+      // int num;
+      // cin >> num;
+
+      // for (int i = 1; i < 11; i++)
+      // {
+      //       cout << i * num << endl;
+      // }
+
+      // ------ second problem related to odd and even
+      // int num;
+      // cin >> num;
+
+      // for (int i = 1; i < num; i++)
+      // {
+      //       if (i % 2 == 0)
+      //       {
+      //             cout << i << " - even" << endl;
+      //       }
+      //       else
+      //       {
+      //             cout << i << " - odd" << endl;
+      //       }
+      // }
+
+      //------- third problem related to sum ----------
+      // int num;
+      // cin >> num;
+
+      // int sum = 0;
+
+      // //  --------- using for loop -------
+      // // for (int i = 1; i < num; i++)
+      // // {
+      // //       sum = sum + i;
+      // // }
+
+      // // ---- using while loop ------
+      // int i = 1;
+      // while (i < num)
+      // {
+      //       sum = sum + i;
+      //       i++;
+      // }
+
+      // cout << "total = " << sum << endl;
+
+      // ------------ forth problem --------
+      // int num;
+      // cin >> num;
+
+      // int sum = 0;
+
+      // //  --------- using for loop -------
+      // // for (int i = 1; i < num; i++)
+      // // {
+      // //       sum = sum + i;
+      // // }
+
+      // // ---- using while loop ------
+      // int i = 1;
+      // while (i < num)
+      // {
+      //       sum = sum + i;
+      //       i++;
+      // }
+
+      return 0;
+}
