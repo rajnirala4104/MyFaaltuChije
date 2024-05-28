d1fc3e2f1db3c3431e2c45845d10eb837b80aee7
Raj_Nirala <83572606+rajnirala4104@users.noreply.github.com>
2024-05-28 23:01:05 +0530

    math functions

---
+++ learn/C++/mathInCpp.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    cout << max(23, 10) << endl;  // max(a, b) -> to find a largest number from two numbers
+    cout << min(100, 40) << endl; // min(a,b) -> to find a smallest number from two numbers
+    cout << sqrt(64) << endl;     // to find a square root of a number
+    cout << round(2.5);           // to find round figure of a number e.g -> 2.4 or less then thisz would be 2 and 2.5 or more then this would be 3
+
+    return 0;
+}
