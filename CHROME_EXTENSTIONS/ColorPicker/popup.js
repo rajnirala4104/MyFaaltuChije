1ab9c40a49db650901a30553fb4af44d78d26da7
Raj_Nirala <83572606+rajnirala4104@users.noreply.github.com>
2024-06-12 22:27:15 +0530

    used chrome api

--- CHROME_EXTENSTIONS/ColorPicker/popup.js
+++ CHROME_EXTENSTIONS/ColorPicker/popup.js
@@ -1,6 +1,30 @@
 console.log("running..");
 
 const pickBtn = document.getElementById('pickBtn');
-pickBtn.addEventListener('click', () => {
-    console.log("clicking..");
+pickBtn.addEventListener('click', async () => {
+    const [tab] = await chrome.tabs.query({
+        active: true,
+        currentWindow: true
+    });
+
+    // injecting a script to current tab
+    chrome.scripting.executeScript({
+        // current tab id which is coming from chrome.tab api
+        target: { tabId: tab.id },
+
+        // a function that we want to run in current tab
+        function: pickColor
+    })
 });
+
+async function pickColor() {
+    try {
+
+        const eyeDropper = new window.EyeDropper();
+        const selectedColor = await eyeDropper.open();
+        console.log(selectedColor);
+
+    } catch (error) {
+        console.log(error)
+    }
+}
