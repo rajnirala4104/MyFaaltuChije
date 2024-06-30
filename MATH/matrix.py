b03cb72a3d0ad2c5fd02af2b3c5553065ab6e0ba
Raj_Nirala <83572606+rajnirala4104@users.noreply.github.com>
2024-06-30 15:49:13 +0530

    matrix in python

--- MATH/matrix.py
+++ MATH/matrix.py
@@ -1,6 +1,15 @@
 demo_metrix = [[11, 12, 13], [21, 22, 23], [31, 32, 33], [41, 42, 43]]
 
+# Creating an empty list
+column = []
 for row in demo_metrix:
-   print(row)
-   # for col in row:
-      # print(col)
+   # Printing each row
+   print(f"row{demo_metrix.index(row)}: {row}")
+   # checking if current row index is less than the lenght of the current row
+   if demo_metrix.index(row) < len(demo_metrix)-1:
+      # using append() function to add the current row to the column and map() function to iterate over the current row elements
+      column.append(list(map(lambda x: x[demo_metrix.index(row)], demo_metrix)))
+
+print(column)
+
+
