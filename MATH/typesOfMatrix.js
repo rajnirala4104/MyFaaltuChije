145adb479208614404ee43bae6a1e6f2be087211
Raj_Nirala <83572606+rajnirala4104@users.noreply.github.com>
2024-07-01 22:02:47 +0530

    added some matrixes

---
+++ MATH/typesOfMatrix.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ * MxN (M=rows, N=columns)
+ * e.g. 4x1 (M=4, N=1) etc
+ * 
+ */
+
+let columnMatrix = [[1], [2], [3], [4]]; // 4x1 column matrix has one column and multiple rows
+let rowMatrix = [[1, 2, 3, 4]]; // 1x4 row matrix has one row and multiple columns
+let squareMatix = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]; //  4x4 square matrix has equal number of rows and columns
+let diagonalMatrix = [[1, 0, 0, 0], [0, 2, 0, 0], [0, 0, 3, 0], [0, 0, 0, 4]]; // diagonal matrixs are always square matrix (4x4) and have only one diagonal elements (1, 2, 3, 4) in it in 2D array.
+let scalerMatrix = [[2, 0, 0], [0, 2, 0], [0, 0, 2]]; // scaler matrix are always square matrix (3x3) and have only one and equal diagonal elements (2, 2, 2) in it in 2D array.
+let identityMatrix = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]; // identity matrix are always square matrix (3x3) and have only one diagonal elements (1, 1, 1) in it in 2D array.
+let symetricMatrix = [[1, 2, 3], [2, 4, 5], [3, 5, 6]]; // symetric matrix are always square matrix (3x3) and have only one and equal diagonal elements (1, 4, 6) in it in 2D array.
+let zeroMatrix = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]; // 3x3 zero matrix has all elements zero (0, 0, 0) in it.
+
---
+++ MATH/typesOfMatrix.py
@@ -0,0 +1,10 @@
+# types of matrix
+general_matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] # General matrix
+square_matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] # Square matrix - are equal if the number of rows and columns are equal 
+diagonal_matrix = [[1, 0, 0, 0], [0, 2, 0, 0], [0, 0, 3, 0], [0, 0, 0, 4]] # Diagonal matrix - are equal if the diagonal elements are equal
+symetric_matrix = [[1, 2, 3], [2, 4, 5], [3, 5, 6]] #Symmetric matrix - are equal if the transpose of the matrix is equal to itself
+identity_matrix = [[1, 0, 0], [0, 1, 0], [0, 0, 1]] # Identity matrix - 3x3 
+scaler_matrix = [[2, 0, 0], [0, 2, 0], [0, 0, 2]] # scaler matrix are equal if the product of the matrix and the scaler is equal
+zero_matrix = [[0, 0, 0], [0, 0, 0], [0, 0, 0]] # zero matrix has all the elements equal to zero
+coulmn_matrix = [[1], [2],[3],[4]] # 4x1 column matrix has one column and multiple rows
+row_matrix = [[1,2,3,4]] # 1x4 row matrix has one row and multiple columns
