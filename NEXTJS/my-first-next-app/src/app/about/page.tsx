8885fe1e23e87613b55889a1e320975e6731ca96
Raj_Nirala <83572606+rajnirala4104@users.noreply.github.com>
2024-07-13 16:34:26 +0530

    feat: use static and dynamic metadata

--- NEXTJS/my-first-next-app/src/app/about/page.tsx
+++ NEXTJS/my-first-next-app/src/app/about/page.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
+export const metadata = {
+   title: "About Page"
+}
+
 export default function AboutPage() {
    return (
       <section className="w-full h-full flex justify-center items-center">
--- NEXTJS/my-first-next-app/src/app/users/[userId]/reviews/[reviewId]/page.tsx
+++ NEXTJS/my-first-next-app/src/app/users/[userId]/reviews/[reviewId]/page.tsx
@@ -1,4 +1,38 @@
 import React from 'react'
+import { Metadata } from 'next'
+
+interface props {
+   params: {
+      userId: string,
+      reviewId: string
+   }
+}
+
+/**
+ * This function generates the metadata for the page. Metadata is used by Next.js
+ * to provide the title, description, and other information to the browser.
+ *
+ * The function takes an object as a parameter, which contains the parameters
+ * for the page. In this case, the parameters are the userId and reviewId.
+ *
+ * The function returns an object that contains the title of the page. The title
+ * is a string that is used by the browser to display the title of the page in
+ * the tab and in the search engines.
+ *
+ * @param {props} params - An object that contains the parameters for the page.
+ * @return {Metadata} An object that contains the title of the page.
+ */
+export const generateMetadata = ({ params }: props): Metadata => {
+   /**
+    * The title of the page. This is a string that is used by the browser to
+    * display the title of the page in the tab and in the search engines.
+    *
+    * @type {string}
+    */
+   return {
+      title: `User ${params.userId} Review ${params.reviewId}`
+   }
+}
 
 export default function Review({ params }: {
    params: {
