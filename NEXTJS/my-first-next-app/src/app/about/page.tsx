ce2559b8737a5cbd4b291747acd8bb9066574ffe
Raj_Nirala <83572606+rajnirala4104@users.noreply.github.com>
2024-07-13 16:45:04 +0530

    more about metadeta

--- NEXTJS/my-first-next-app/src/app/about/page.tsx
+++ NEXTJS/my-first-next-app/src/app/about/page.tsx
@@ -1,7 +1,10 @@
+import { Metadata } from 'next'
 import React from 'react'
 
-export const metadata = {
-   title: "About Page"
+export const metadata: Metadata = {
+   title: {
+      absolute: "About Page",
+   }
 }
 
 export default function AboutPage() {
