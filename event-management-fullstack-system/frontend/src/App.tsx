b522de30bb1061547d1e74893d4759739f514f88
Raj_Nirala <83572606+rajnirala4104@users.noreply.github.com>
2025-03-04 10:43:05 +0530

    added a new project

---
+++ event-management-fullstack-system/frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Fragment, useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <Fragment>
+      <div className="text-2xl text-red-500">Hello world</div>
+    </Fragment>
+  )
+}
+
+export default App
